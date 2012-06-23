3651697219af322964f9f61a07cfae8c7f88d88c
Rob Ashton <robashton@codeofrob.com>
2012-06-23 12:21:58 +0200

    First call that actually works

--- server.js
+++ server.js
@@ -1,42 +1,60 @@
 var express = require('express')
-,   everyauth = require('everyauth')
 ,   https = require('https')
+,   passport = require('passport')
+,   GitHubStrategy = require('passport-github').Strategy
 
 var users = {}
 
-everyauth.github
-  .appId('ad2622e4ecf41b39e8eb')
-  .appSecret('9b651769f9c63fa8707148b3c41e69fdb6bf41ba')
-  .moduleTimeout(-1)
-  .findOrCreateUser(function (session, accessToken, accessTokenExtra, githubUserMetadata) {
-    return users[githubUserMetadata.id] || (users[githubUserMetadata.id] =  githubUserMetadata)
-   })
-   .redirectPath('/')
+passport.serializeUser(function(user, done) {
+   done(null, user);
+});
 
-everyauth
-  .everymodule
-  .findUserById(function(userId, cb) {
-    cb(users[userId])
-  })
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+      clientID: 'ad2622e4ecf41b39e8eb',
+      clientSecret: '9b651769f9c63fa8707148b3c41e69fdb6bf41ba',
+      callbackURL: "http://localhost:8080/auth/github/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+      profile.token = accessToken
+      return done(null, profile)
+    }));
+
 var server = express.createServer()
+
 server.configure(function() {
   server.use(express.static('site'))
   server.use(express.bodyParser())
   server.use(express.cookieParser())
   server.use(express.session({secret : 'ssshlol' }))
-  server.use(everyauth.middleware())
+  server.use(express.methodOverride());
+  server.use(passport.initialize());
+  server.use(passport.session());
   server.use(server.router)
 })
 server.listen(8080)
 
+server.get('/auth/github',
+  passport.authenticate('github'),
+  function(req, res){ });
+
+server.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login'}),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+
 server.get('/issues/all', function(req, res) {
-  makeGithubRequest('/repos', req.user, function(err, data) {
+  makeGithubRequest('/user/repos', req.user, function(err, data) {
     res.send(data)
-  })
+ })
 });
 
 function makeGithubRequest(path, user, cb) {
-  console.log(user)
  var request = https.get({
     host: 'api.github.com', 
     path: path,
@@ -46,11 +64,12 @@ function makeGithubRequest(path, user, cb) {
   }, 
   function(res) {
     var data = '';
+
     res.on('data', function (chunk) {
       data += chunk;
     });
     res.on('end', function() {
-      cb(null, JSON.parse(data));
+      cb(null, data);
     });
   })
   .on('error', function(e) {
